b64b409b05fb4728866122034e545ee30bd20950
Fernando Rodríguez <ferrodest1999@gmail.com>
2024-07-26 17:17:59 +0200

    Textos proyectos

--- src/db/Proyectos.ts
+++ src/db/Proyectos.ts
@@ -10,7 +10,7 @@ import icono_lemonade from "@img/proyectos/icono_lemonade.svg";
 import icono_todo from "@img/proyectos/icono_todo.png";
 import icono_pokemon from "@img/proyectos/icono_pokemon.png";
 import icono_drumpads from "@img/proyectos/icono_drumpads.svg";
-import { getSkills, Skill } from "./SkillsInfinteScroll";
+import { getSkills, Skill } from "./Skills";
 
 export interface Proyecto {
   nombre: string;
@@ -32,7 +32,7 @@ export const Proyectos: Proyecto[] = [
     path:'lemonade',
     descripcionCorta: `Aplicación dirigida a productores musicales. Conjunto de herramientas para obtener metadatos de canciones de Spotify con el fin de crear remixes o 'mash-ups'.`,
     descripcionLarga:[
-      'Lemonade es el primer proyecto que desarrollé con React, como proyecto de final de grado durante mi Erasmus en Polonia.',
+      'Lemonade es la primera aplicación que desarrollé con React, como proyecto de final de grado durante mi Erasmus en Polonia.',
       'Haciendo uso del API de Spotify se obtiene información de las canciones y/o artistas que elige el usuario para crear posibles emparejamientos de canciones compatibles, con el fin de realizar remixes, mash-ups o simplemente consultar información musical de cierta canción.',
       'Este proyecto me hizo comprender la necesidad de realizar un análisis previo al desarrollo, tanto a nivel de funcionalidades como de diseño.'
 
@@ -45,7 +45,7 @@ export const Proyectos: Proyecto[] = [
     skills: getSkills(['react','css','cypress']),
     caracteristicas:[
       'Utiliza el API de Spotify para obtener los datos de las canciones.', 
-      'Traducida al inglés con i18next.', 
+      'Gestión de traducciones con i18next.', 
       'Realizados sencillos tests E2E con Cypress'
     ]
   },
@@ -53,31 +53,40 @@ export const Proyectos: Proyecto[] = [
     nombre: "KrumDit",
     path:'krumdit',
     descripcionCorta: `Drumpads con una interfaz de usuario moderna y una experiencia de usuario agradable.`,
-    descripcionLarga:[],
+    descripcionLarga:[
+      `Se trata de una sencilla pero atractiva aplicación para tocar ritmos con las teclas del teclado o la pantalla de tu móvil.`,
+      `Permite programar ritmos gracias a un secenciador de ritmos.`,
+      `Cuenta con presets predefinidos según géneros musicales y permite crear tus propios conujuntos de sonidos.`
+    ],
     icono: icono_drumpads,
     imagen_pc: img_krumdit_pc,
     imagen_movil: img_krumdit_movil,
     urlGithub: "https://github.com/SurvivingCuriosity/drumpads",
     urlProyecto: "https://krumdit.com/",
-    skills: getSkills(['react','tailwind']),
+    skills: getSkills(['react','tailwind','typescript','Context API','Custom Hooks']),
     caracteristicas:[
       'Baja latencia en la reproducción de sonidos gracias a la utilización AudioContext.',
-      'Diseño adaptado a todas las resoluciones de pantalla.'
+      'Diseño adaptado a todas las resoluciones de pantalla.',
+      `Gestión del estado de la aplicación con Context. Estilos y gestión de temas con Tailwind y variables CSS.`
     ]
   },
   {
     nombre: "Simple TO-DO",
     path:'todo',
     descripcionCorta: `Aplicación de tareas con aspecto minimalista. Anidamiento mediante subtareas y categorías. Realizados tests de usabilidad analizando el uso que hace de ella mi madre :)`,
-    descripcionLarga:[],
+    descripcionLarga:[
+      `Aplicación desarrollada para uso personal (me encantan las aplicaciones de tareas)`,
+      `Interfaz sencilla, anidamiento mediante categorías y subtareas.`
+    ],
     icono: icono_todo,
     imagen_pc: img_todo_pc,
     imagen_movil: img_todo_movil,
     urlGithub: "https://github.com/SurvivingCuriosity/simple-todo-byFer",
     urlProyecto: "https://todo-byfer.vercel.app/",
-    skills: getSkills(['react','tailwind']),
+    skills: getSkills(['react','tailwind','Context API']),
     caracteristicas:[
-      'PWA con funcionamiento offline.'
+      'PWA con funcionamiento offline.',
+      `Gestión de estado global mediante el uso de Context y useReducer.`
     ]
   },
   {
@@ -91,6 +100,8 @@ export const Proyectos: Proyecto[] = [
     urlGithub: "https://github.com/SurvivingCuriosity/pokemon_RPR",
     urlProyecto: "https://pokemon-ruper.vercel.app/",
     skills: getSkills(['react','redux','css']),
-    caracteristicas:[]
+    caracteristicas:[
+      `Gestión del estado global con Redux.`,
+    ]
   },
 ];
